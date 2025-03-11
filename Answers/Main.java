daa9acdbed4659f6874564fbec0f43920505b1b2
pooyancoder <poyan.moh1384@gmail.com>
2025-03-11 15:20:10 +0330

    git problem

---
+++ Answers/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.random;
+
+
+
+
+
+public class Main{
+
+
+
+    Grid p1MainGrid = new Grid();    
+    Grid p2MainGrid = new grid();
+    Grid p1AttackGrid = new Grid();
+    Grid p2AttackGrid = new Grid();
+
+    p1MainGrid.shapingGrid();
+    p2MainGrid.shapingGrid();
+    p1AttackGrid.shapingGrid();
+    p2AttackGrid.shapingGrid();
+
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        random randomString = new random();
+
+    }
+
+    void placeShip(Grid grid , Ship ship){
+        
+    }
+}
